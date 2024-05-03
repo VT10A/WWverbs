8d78c41c207dd31c4afd5337ad905f85326c1799
VT10A <152657392+VT10A@users.noreply.github.com>
2024-05-03 12:08:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ tab1, tab2, tab3 = st.tabs(["Topic frequency", "Summaries", "Relative difference
 st.sidebar.title("Filters")
 
 # Dropdown to select the data file
-selected_data = st.sidebar.selectbox("Select verbatim:", ["Q6b Negative", "Q14"])
+selected_data = st.sidebar.selectbox("Select verbatim:", ["Q6b Negative", "Q14","Q6a Positive"])
 
 
 # Mapping between dropdown selection and corresponding CSV file
-file_mapping = {"Q6b Negative": "6bNegative.csv", "Q14": "Q14.csv"}
+file_mapping = {"Q6b Negative": "6bNegative.csv", "Q14": "Q14.csv","Q6a Positive":"Q6aPositive.csv"}
 
 # Load the selected data
 data = load_data(file_mapping[selected_data])
